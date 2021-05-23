5fcaeb135903d345ea69e1d1f25b332ca7e3c5c1
Eli Inskeep <74996506+EliInskeep@users.noreply.github.com>
2021-05-23 07:23:34 -0500

    CSS Animations
    Added some animations to a couple of things and played around with adding a JS animation for the hero image. Decided to keep it out for the time being but commented it out for now.

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+const hero = document.getElementById('hero')
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+// const tl = new TimelineMax();
+
+// tl.fromTo(hero,3, {height: "0%"}, {height: "80%"});
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
